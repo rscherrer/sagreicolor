fdc54aec7c07c67f4e659b5dae7422ad536b4be2
R. Scherrer <p278834@WORKSPACE.RUG.NL>
2019-02-27 15:16:12 +0100

    can now choose to keep islands that have some but not all wanted habitats

--- R/subset_habitats.R
+++ R/subset_habitats.R
@@ -4,26 +4,36 @@
 #'
 #' @param specdata A data frame containing at least columns for the dependent variables, as well as a column "island" and a column "habitat".
 #' @param whatHabitats A character vector. What habitats to retain?
+#' @param inclusive Do we include the islands with some but not all of the wanted habitats (Conception)? If \code{FALSE}, keep only the islands with all the wanted habitats.
 #' @return A data frame, a subset of \code{specdata}.
 #' @author Raphael Scherrer
 #' @export
 
 # Function to keep only some habitats in the data, to avoid empty cells in the two-way design
-subset_habitats <- function(specdata, whatHabitats) {
-  
-  # Remove islands that do not have all the habitats
-  # For each island...
-  whatIslands <- sapply(levels(specdata$island), function(curr.island) {
-    
-    # Are all habitats in the list present on that island?
-    all(whatHabitats %in% levels(droplevels(specdata[specdata$island == curr.island,]$habitat)))
-    
-  })
-  whatIslands <- names(whatIslands)[whatIslands]
-  
-  # Subset of the data
-  specdata <- subset(specdata, island %in% whatIslands & habitat %in% whatHabitats)
-  
+subset_habitats <- function(specdata, whatHabitats, inclusive = F) {
+
+  # Keep all islands that have at least the wanted habitats
+  if(inclusive) {
+
+    specdata <- subset(specdata, habitat %in% whatHabitats)
+
+  } else {
+
+    # Or keep only the islands with all wanted habitats
+    # For each island...
+    whatIslands <- sapply(levels(specdata$island), function(curr.island) {
+
+      # Are all habitats in the list present on that island?
+      all(whatHabitats %in% levels(droplevels(specdata[specdata$island == curr.island,]$habitat)))
+
+    })
+    whatIslands <- names(whatIslands)[whatIslands]
+
+    # Subset of the data
+    specdata <- subset(specdata, island %in% whatIslands & habitat %in% whatHabitats)
+
+  }
+
   return(droplevels(specdata))
-  
+
 }
